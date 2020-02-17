62c194169ffcf0f52981e929ad39b79ed63dad7b
VitorOM2 <viktormarque2053@gmail.com>
2020-02-16 23:06:38 -0300

    Alterado o botão que reagira a tecla enter

--- ExemploBotao/src/exemplobotao/ExemploBotao.java
+++ ExemploBotao/src/exemplobotao/ExemploBotao.java
@@ -28,7 +28,7 @@ public class ExemploBotao extends JFrame
         botao2.setMnemonic(KeyEvent.VK_V);
         botao3.setMnemonic(KeyEvent.VK_X);
         botao4.setMnemonic(KeyEvent.VK_S);
-        getRootPane().setDefaultButton(botao4);
+        getRootPane().setDefaultButton(botao1);
         
         botao1.setBounds(50,20,100,20);
         botao2.setBounds(50,60,100,20);
