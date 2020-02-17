a36f8d1bced72f301c5c25b0f6daf62d66438744
VitorOM2 <viktormarque2053@gmail.com>
2020-02-16 22:39:29 -0300

    Alterado a descrição de um botão

--- ExemploBotao/src/exemplobotao/ExemploBotao.java
+++ ExemploBotao/src/exemplobotao/ExemploBotao.java
@@ -21,7 +21,7 @@ public class ExemploBotao extends JFrame
         botao1 = new JButton ("Procurar");
         botao2 = new JButton ("Voltar >>");
         botao3 = new JButton ("Próximo >>");
-        botao4 = new JButton ("Abrir");
+        botao4 = new JButton ("Sair");
         
         botao1.setBounds(50,20,100,20);
         botao2.setBounds(50,60,100,20);
